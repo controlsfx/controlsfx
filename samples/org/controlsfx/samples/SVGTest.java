fbc6ddc2089809a030dcc917cc1a063f52735517
Jonathan Giles <jonathan@jonathangiles.net>
2013-04-07 14:33:24 +1200

    Various explorations in SVG support.

--- samples/org/controlsfx/samples/SVGTest.java
+++ samples/org/controlsfx/samples/SVGTest.java
@@ -5,44 +5,52 @@ import java.net.URL;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
 import org.controlsfx.tools.SVGLoader;
 
 public class SVGTest extends Application {
+    
+    private static final int IMAGE_HEIGHT = 175;
+    private static final int IMAGE_SPACING = 0;
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override public void start(Stage primaryStage) throws Exception {
-        final URL pathToFile = SVGTest.class.getResource("error.svg").toURI().toURL();
+        VBox container = new VBox(IMAGE_SPACING);
+        
+        container.setStyle("-fx-background-color: lightblue");
+        
+        final String[] images = new String[] {
+//                "emblem-important.svg",
+//                "error.svg",
+                "help-browser.svg"
+        };
+        
+        for (String svg : images) {
+            loadImage(svg, container);
+        }
 
-        final StackPane root = new StackPane();
-        
-        final Rectangle rect = new Rectangle(400, 400, Color.LIGHTBLUE);
-        
-        SVGLoader.loadSVGImage(pathToFile, 350, -1, new Callback<ImageView, Void>() {
+        Scene scene = new Scene(container);
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(200);
+        primaryStage.setHeight((IMAGE_HEIGHT + IMAGE_SPACING) * (images.length) + IMAGE_HEIGHT / 2.0);
+
+        primaryStage.show();
+    }
+    
+    private void loadImage(final String filename, final VBox container) throws Exception {
+        final URL pathToFile = SVGTest.class.getResource(filename).toURI().toURL();
+
+        SVGLoader.loadSVGImage(pathToFile, IMAGE_HEIGHT, -1, new Callback<ImageView, Void>() {
             @Override public Void call(ImageView image) {
-                root.getChildren().setAll(rect, image);
+                container.getChildren().addAll(image);
                 return null;
             }
         });
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setWidth(400);
-        primaryStage.setHeight(400);
-
-        primaryStage.show();
     }
 }
--- src/org/controlsfx/tools/SVGLoader.java
+++ src/org/controlsfx/tools/SVGLoader.java
@@ -1,6 +1,14 @@
 package org.controlsfx.tools;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.URL;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 import javafx.beans.value.ChangeListener;
 import javafx.concurrent.Worker.State;
@@ -15,6 +23,16 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.html.HTMLImageElement;
 
 import com.sun.javafx.webkit.Accessor;
 import com.sun.webkit.WebPage;
@@ -47,7 +65,10 @@ public class SVGLoader {
      *      image is loaded, where the {@link ImageView} containing the rendered
      *      image will be available.
      */
-    public static void loadSVGImage(URL svgImage, final double prefWidth, final double prefHeight, final Callback<ImageView, Void> callback) {
+    public static void loadSVGImage(final URL svgImage, 
+                                    final double prefWidth, 
+                                    final double prefHeight, 
+                                    final Callback<ImageView, Void> callback) {
         loadSVGImage(svgImage, prefWidth, prefHeight, callback, null);
     }
     
@@ -61,7 +82,8 @@ public class SVGLoader {
      *      been rendered (it will not happen synchronously).
      * @throws NullPointerException The outputImage argument must be non-null.
      */
-    public static void loadSVGImage(URL svgImage, final WritableImage outputImage) {
+    public static void loadSVGImage(final URL svgImage, 
+                                    final WritableImage outputImage) {
         if (outputImage == null) {
             throw new NullPointerException("outputImage can not be null");
         }
@@ -70,36 +92,59 @@ public class SVGLoader {
         loadSVGImage(svgImage, w, h, null, outputImage);
     }
     
-    public static void loadSVGImage(URL svgImage, final double prefWidth, final double prefHeight, final Callback<ImageView, Void> callback, final WritableImage outputImage) {
+    public static void loadSVGImage(final URL svgImage, 
+                                    final double prefWidth, 
+                                    final double prefHeight, 
+                                    final Callback<ImageView, Void> callback, 
+                                    final WritableImage outputImage) {
         final WebView view = new WebView();
         final WebEngine eng = view.getEngine();
         
         // using non-public API to ensure background transparency
-        WebPage webPage = Accessor.getPageFor(eng);
-        webPage.setBackgroundColor(webPage.getMainFrame(), 0xffffff00);
-        webPage.setOpaque(webPage.getMainFrame(), false); 
+        final WebPage webPage = Accessor.getPageFor(eng);
+//        webPage.setBackgroundColor(webPage.getMainFrame(), 0xffffff00);
+//        webPage.setOpaque(webPage.getMainFrame(), false); 
         // end of non-public API
 
         // temporary scene / stage
         final Scene scene = new Scene(view);
         final Stage stage = new Stage();
         stage.setScene(scene);
-        stage.setWidth(0);
-        stage.setHeight(0);
-        stage.setOpacity(0);
+//        stage.setWidth(0);
+//        stage.setHeight(0);
+//        stage.setOpacity(0);
         stage.show();
+        
+//        String svgString = readFile(svgImage);
 
-        String content = 
-                "<body style=\"margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; padding: 0;\">" +
-                "<img width=\"" + prefWidth + "\" height=\"" + prefHeight + "\" src=\"" + svgImage.toExternalForm() + "\" />";
+        String content =
+        "<html>" +
+            "<body style=\"margin-top: 0px; margin-bottom: 30px; margin-left: 0px; margin-right: 0px; padding: 0;\">" +
+//                "<div style=\"width: " + prefWidth + "; height: " + prefHeight + ";\">" +
+                    "<img id=\"svgImage\" style=\"display: block;float: top;\" width=\"" + prefWidth + "\" height=\"" + prefHeight + "\" src=\"" + svgImage.toExternalForm() + "\" />" +
+//                    svgString +
+//                "</div>" +
+            "</body>" +
+        "</head>";
+                
+        
+        System.out.println(content);
+        
         eng.loadContent(content);
-
+        
         eng.getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
             @Override public void changed(javafx.beans.value.ObservableValue<? extends State> o, State oldValue, State newValue) {
                 if (newValue == State.SUCCEEDED) {
+                    
+//                    HTMLImageElement svgImageElement = (HTMLImageElement) getSvgDom(eng);
+//                    System.out.println(svgImageElement.getAttributes());
+                    
+                    final double svgWidth = prefWidth >= 0 ? prefWidth : getSvgWidth(eng);
+                    final double svgHeight = prefHeight >= 0 ? prefWidth : getSvgHeight(eng);
+                    
                     SnapshotParameters params = new SnapshotParameters();
                     params.setFill(Color.TRANSPARENT);
-                    params.setViewport(new Rectangle2D(0, 0, prefWidth, prefWidth));
+                    params.setViewport(new Rectangle2D(0, 0, svgWidth, svgHeight));
                     
                     view.snapshot(new Callback<SnapshotResult, Void>() {
                         @Override public Void call(SnapshotResult param) {
@@ -110,7 +155,7 @@ public class SVGLoader {
                                 callback.call(image);
                             }
                             
-                            stage.hide();
+//                            stage.hide();
                             return null;
                         }
                     }, params, outputImage);
@@ -118,4 +163,58 @@ public class SVGLoader {
             }
         });
     }
+    
+//    private static String readFile(URL url) {
+//        try {
+//            FileInputStream stream = new FileInputStream(new File(url.toURI()));
+//            try {
+//                FileChannel fc = stream.getChannel();
+//                MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+//                return Charset.defaultCharset().decode(bb).toString();
+//            }
+//            finally {
+//                stream.close();
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+    
+    private static double getSvgWidth(WebEngine webEngine) {
+        Object result = getSvgDomProperty(webEngine, "offsetWidth");
+        if (result instanceof Integer) {
+            return (Integer) result;
+        }
+        return -1;
+    }
+    
+    private static double getSvgHeight(WebEngine webEngine) {
+        Object result = getSvgDomProperty(webEngine, "offsetHeight");
+        if (result instanceof Integer) {
+            return (Integer) result;
+        }
+        return -1;
+    }
+    
+    private static Object getSvgDomProperty(final WebEngine webEngine, final String property) {
+        return webEngine.executeScript("document.getElementById('svgImage')." + property);
+    }
+
+//    private static HTMLImageElement getSvgDom(WebEngine webEngine) {
+//        return (HTMLImageElement) webEngine.executeScript("document.getElementById('svgImage')");
+//    }
+//    
+//    private static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
+//        TransformerFactory tf = TransformerFactory.newInstance();
+//        Transformer transformer = tf.newTransformer();
+//        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+//        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+//        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+//        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+//        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+//
+//        transformer.transform(new DOMSource(doc), 
+//             new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+//    }
 }

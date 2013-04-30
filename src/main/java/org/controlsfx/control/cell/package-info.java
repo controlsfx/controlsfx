2588d46380187cd8ce0a0f14d07731aa536c8f41
Jonathan Giles <jonathan@jonathangiles.net>
2013-04-30 20:05:24 +1200

    Added basic package-info.java files to the public API packages.

---
+++ src/main/java/org/controlsfx/control/cell/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A package containing a number of useful cell-related classes that do not
+ * exist in the base JavaFX distribution, many related to the new 
+ * {@link org.controlsfx.control.GridView GridView} control offered in 
+ * ControlsFX.
+ * 
+ * @see org.controlsfx.control.GridView
+ */
+package org.controlsfx.control.cell;
---
+++ src/main/java/org/controlsfx/tools/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A package containing a number of useful utility methods.
+ */
+package org.controlsfx.tools;

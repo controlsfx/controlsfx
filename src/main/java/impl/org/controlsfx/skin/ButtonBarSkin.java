ac93b2096e66ae8daebb1c8feeae6e4aa7dea548
Jonathan Giles <jonathan@jonathangiles.net>
2013-05-03 17:55:52 +1200

    Convert the ButtonBar to a fully-fledged Control. This saves us from extending from HBox, which means the getChildren() method is no longer public and it should now be clearer how to use the control.

---
+++ src/main/java/impl/org/controlsfx/skin/ButtonBarSkin.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright (c) 2013, ControlsFX
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ControlsFX, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CONTROLSFX BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package impl.org.controlsfx.skin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javafx.collections.ListChangeListener;
+import javafx.scene.Node;
+import javafx.scene.control.ButtonBase;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+
+import org.controlsfx.control.ButtonBar;
+import org.controlsfx.control.ButtonBar.ButtonType;
+
+import com.sun.javafx.scene.control.behavior.BehaviorBase;
+import com.sun.javafx.scene.control.skin.BehaviorSkinBase;
+
+public class ButtonBarSkin extends BehaviorSkinBase<ButtonBar, BehaviorBase<ButtonBar>> {
+    
+    /**************************************************************************
+     * 
+     * Static fields
+     * 
+     **************************************************************************/
+
+    private static String CATEGORIZED_TYPES = "LRHEYNXBIACO";
+    
+    // represented as a ButtonType
+    public static String BUTTON_TYPE_PROPERTY = "controlfx.button.type";
+    
+    
+    
+    /**************************************************************************
+     * 
+     * fields
+     * 
+     **************************************************************************/
+    
+    private HBox hbox;
+    
+    
+    
+    /**************************************************************************
+     * 
+     * Constructors
+     * 
+     **************************************************************************/
+
+    public ButtonBarSkin(final ButtonBar control) {
+        super(control, new BehaviorBase<>(control));
+        
+        // TODO: the gap has to be OS dependent
+        this.hbox = new HBox(10);
+        getChildren().add(hbox);
+        
+        layoutButtons();
+        
+        control.getButtons().addListener( new ListChangeListener<ButtonBase>() {
+            @Override public void onChanged(ListChangeListener.Change<? extends ButtonBase> change) {
+                layoutButtons();
+            }
+        });
+        
+        registerChangeListener(control.buttonOrderProperty(), "BUTTON_ORDER");
+    }
+    
+    
+    
+    /**************************************************************************
+     * 
+     * Overriding public API
+     * 
+     **************************************************************************/
+    
+    @Override protected void handleControlPropertyChanged(String p) {
+        super.handleControlPropertyChanged(p);
+        
+        if ("BUTTON_ORDER".equals(p)) {
+            layoutButtons();
+        }
+    }
+    
+    
+    
+    /**************************************************************************
+     * 
+     * Implementation
+     * 
+     **************************************************************************/
+    
+    private void layoutButtons() {
+        List<? extends ButtonBase> buttons = getSkinnable().getButtons();
+        
+        hbox.getChildren().clear();
+        
+        Map<String, List<ButtonBase>> buttonMap = buildButtonMap(buttons);
+        SpacerType preparedSpacer = SpacerType.NONE;
+        for ( char type: getSkinnable().getButtonOrder().toCharArray()) {
+            
+            if ( type == '+') {
+               preparedSpacer = preparedSpacer.replace(SpacerType.DYNAMIC);
+            } else if ( type == '_') {
+               preparedSpacer = preparedSpacer.replace(SpacerType.FIXED);
+            } else {
+                
+                List<ButtonBase> buttonList = buttonMap.get(String.valueOf(type).toUpperCase());
+                if ( buttonList != null ) {
+                    Node spacer = preparedSpacer.create();
+                    if ( spacer != null ) {
+                        hbox.getChildren().add(spacer);
+                        preparedSpacer = preparedSpacer.replace(SpacerType.NONE);
+                    }
+                    
+                    for( ButtonBase btn: buttonList) {
+                        hbox.getChildren().add(btn);
+                        HBox.setHgrow(btn, Priority.ALWAYS);
+                    }
+                } 
+            }
+        }
+        
+    }
+    
+    private String getButtonType( ButtonBase btn ) {
+        ButtonType buttonType =  (ButtonType) btn.getProperties().get(BUTTON_TYPE_PROPERTY);
+        String typeCode = buttonType.getTypeCode();
+        typeCode = typeCode.length() > 0? typeCode.substring(0,1): "";
+        return CATEGORIZED_TYPES.contains(typeCode.toUpperCase())? typeCode : "U"; 
+    }
+    
+    private Map<String, List<ButtonBase>> buildButtonMap( List<? extends ButtonBase> buttons ) {
+        
+        Map<String, List<ButtonBase>> buttonMap = new HashMap<>();
+        for (ButtonBase btn : buttons) {
+            if ( btn == null ) continue;
+            String type =  getButtonType(btn); 
+            List<ButtonBase> typedButtons = buttonMap.get(type);
+            if ( typedButtons == null ) {
+                typedButtons = new ArrayList<ButtonBase>();
+                buttonMap.put(type, typedButtons);
+            }
+            typedButtons.add( btn );
+        }
+        return buttonMap;
+        
+    }
+
+    
+    
+    /**************************************************************************
+     * 
+     * Support classes / enums
+     * 
+     **************************************************************************/
+    
+    private enum SpacerType {
+        FIXED {
+            @Override public Node create() {
+                Region spacer = new Region();
+                spacer.setMinWidth(10);
+                HBox.setHgrow(spacer, Priority.NEVER);
+                return spacer;
+            }
+            
+            public SpacerType replace( SpacerType type ) {
+                return type == NONE || type == DYNAMIC? type: this;
+            }
+            
+        },
+        DYNAMIC {
+            @Override public Node create() {
+                Region spacer = new Region();
+                HBox.setHgrow(spacer, Priority.ALWAYS);
+                return spacer;
+            }
+            
+            public SpacerType replace( SpacerType type ) {
+                return type == NONE? type: this;
+            }
+        },
+        NONE {
+            public SpacerType replace( SpacerType type ) {
+                return type == FIXED || type == DYNAMIC? type: this;
+            }
+
+        };
+        
+        public abstract SpacerType replace( SpacerType type );
+        
+        public Node create() {
+            return null;
+        }
+    }
+}
--- src/main/java/org/controlsfx/control/ButtonBar.java
+++ src/main/java/org/controlsfx/control/ButtonBar.java
@@ -1,4 +1,32 @@
-package org.controlsfx.tools;
+/**
+ * Copyright (c) 2013, ControlsFX
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ControlsFX, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CONTROLSFX BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.controlsfx.control;
+
+import impl.org.controlsfx.skin.ButtonBarSkin;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,16 +35,20 @@ import java.util.Map;
 
 import com.sun.javafx.Utils;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.ButtonBase;
+import javafx.scene.control.Control;
+import javafx.scene.control.Skin;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 
-public final class ButtonBar extends HBox {
+public final class ButtonBar extends Control {
     
     /**************************************************************************
      * 
@@ -24,11 +56,6 @@ public final class ButtonBar extends HBox {
      * 
      **************************************************************************/
 
-    private static String CATEGORIZED_TYPES = "LRHEYNXBIACO";
-    
-    // represented as a ButtonType
-    private static String BUTTON_TYPE_PROPERTY = "controlfx.button.type";
-    
     /**
      * The default button ordering on Windows.
      */
@@ -134,13 +161,13 @@ public final class ButtonBar extends HBox {
          */
         SMALL_GAP("_");
         
-        private final String type;
+        private final String typeCode;
         private ButtonType(String type) {
-            this.type = type;
+            this.typeCode = type;
         }
         
-        private String getType() {
-            return type;
+        public String getTypeCode() {
+            return typeCode;
         }
     }
     
@@ -153,7 +180,7 @@ public final class ButtonBar extends HBox {
      * @param type The type to designate the button as.
      */
     public static void setType(ButtonBase button, ButtonType type) {
-        button.getProperties().put(BUTTON_TYPE_PROPERTY, type);
+        button.getProperties().put(ButtonBarSkin.BUTTON_TYPE_PROPERTY, type);
     }
     
     
@@ -163,8 +190,6 @@ public final class ButtonBar extends HBox {
      * Private fields
      * 
      **************************************************************************/
-    
-    private String buttonOrder;
     
     private ObservableList<ButtonBase> buttons = FXCollections.<ButtonBase>observableArrayList();
     
@@ -177,14 +202,6 @@ public final class ButtonBar extends HBox {
      **************************************************************************/
     
     public ButtonBar() {
-        // TODO: the gap has to be OS dependent
-        super(10);
-        buttons.addListener( new ListChangeListener<ButtonBase>() {
-            @Override public void onChanged(ListChangeListener.Change<? extends ButtonBase> change) {
-                layoutButtons(buttons);
-            }
-        });
-        
         // set the default button order 
         if (Utils.isMac()) {
             setButtonOrder(BUTTON_ORDER_MAC_OS);
@@ -203,23 +220,53 @@ public final class ButtonBar extends HBox {
      * Public API
      * 
      **************************************************************************/
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override protected Skin<?> createDefaultSkin() {
+        return new ButtonBarSkin(this);
+    }
 
+    /**
+     * Placing buttons inside this ObservableList will instruct the ButtonBar
+     * to position them relative to each other based on their specified
+     * {@link ButtonType}. To set the ButtonType for a button, simply call
+     * {@link ButtonBar#setType(ButtonBase, ButtonType)}, passing in the 
+     * relevant ButtonType.
+     *  
+     * @return A list containing all buttons currently in the button bar, and 
+     *      allowing for further buttons to be added or removed.
+     */
     public ObservableList<ButtonBase> getButtons() {
         return buttons;
     }
     
     
+    
+    /**************************************************************************
+     * 
+     * Properties
+     * 
+     **************************************************************************/
+    
     /**
-     * Sets the order for the typical buttons in a standard button bar. It is 
+     * The order for the typical buttons in a standard button bar. It is 
      * one letter per button type, and the applicable options are part of 
      * {@link ButtonType}. Default button orders for operating systems are also 
      * available: {@link #BUTTON_ORDER_WINDOWS}, {@link #BUTTON_ORDER_MAC_OS},
      * {@link #BUTTON_ORDER_LINUX}.
      */
-    public void setButtonOrder(String buttonOrder) {
-        this.buttonOrder = buttonOrder;
-        
-        layoutButtons(buttons);
+    private final StringProperty buttonOrderProperty = 
+            new SimpleStringProperty(this, "buttonOrder", BUTTON_ORDER_WINDOWS);
+    public final StringProperty buttonOrderProperty() {
+        return buttonOrderProperty;
+    }
+    public final void setButtonOrder(String buttonOrder) {
+        buttonOrderProperty.set(buttonOrder);
+    }
+    public final String getButtonOrder() {
+        return buttonOrderProperty.get();
     }
     
     
@@ -230,61 +277,6 @@ public final class ButtonBar extends HBox {
      * 
      **************************************************************************/
     
-    private void layoutButtons(List<? extends ButtonBase> buttons) {
-        
-        getChildren().clear();
-        
-        Map<String, List<ButtonBase>> buttonMap = buildButtonMap(buttons);
-        SpacerType preparedSpacer = SpacerType.NONE;
-        for ( char type: buttonOrder.toCharArray()) {
-            
-            if ( type == '+') {
-               preparedSpacer = preparedSpacer.replace(SpacerType.DYNAMIC);
-            } else if ( type == '_') {
-               preparedSpacer = preparedSpacer.replace(SpacerType.FIXED);
-            } else {
-                
-                List<ButtonBase> buttonList = buttonMap.get(String.valueOf(type).toUpperCase());
-                if ( buttonList != null ) {
-                    Node spacer = preparedSpacer.create();
-                    if ( spacer != null ) {
-                        getChildren().add(spacer);
-                        preparedSpacer = preparedSpacer.replace(SpacerType.NONE);
-                    }
-                    
-                    for( ButtonBase btn: buttonList) {
-                        getChildren().add(btn);
-                        HBox.setHgrow(btn, Priority.ALWAYS);
-                    }
-                } 
-            }
-        }
-        
-    }
-    
-    private String getButtonType( ButtonBase btn ) {
-        ButtonType buttonType =  (ButtonType) btn.getProperties().get(BUTTON_TYPE_PROPERTY);
-        String typeString = buttonType.getType();
-        typeString = typeString.length() > 0? typeString.substring(0,1): "";
-        return CATEGORIZED_TYPES.contains(typeString.toUpperCase())? typeString: "U"; 
-    }
-    
-    private Map<String, List<ButtonBase>> buildButtonMap( List<? extends ButtonBase> buttons ) {
-        
-        Map<String, List<ButtonBase>> buttonMap = new HashMap<>();
-        for (ButtonBase btn : buttons) {
-            if ( btn == null ) continue;
-            String type =  getButtonType(btn); 
-            List<ButtonBase> typedButtons = buttonMap.get(type);
-            if ( typedButtons == null ) {
-                typedButtons = new ArrayList<ButtonBase>();
-                buttonMap.put(type, typedButtons);
-            }
-            typedButtons.add( btn );
-        }
-        return buttonMap;
-        
-    }
 
     
     
@@ -294,42 +286,4 @@ public final class ButtonBar extends HBox {
      * 
      **************************************************************************/
     
-    private enum SpacerType {
-        FIXED {
-            @Override public Node create() {
-                Region spacer = new Region();
-                spacer.setMinWidth(10);
-                HBox.setHgrow(spacer, Priority.NEVER);
-                return spacer;
-            }
-            
-            public SpacerType replace( SpacerType type ) {
-                return type == NONE || type == DYNAMIC? type: this;
-            }
-            
-        },
-        DYNAMIC {
-            @Override public Node create() {
-                Region spacer = new Region();
-                HBox.setHgrow(spacer, Priority.ALWAYS);
-                return spacer;
-            }
-            
-            public SpacerType replace( SpacerType type ) {
-                return type == NONE? type: this;
-            }
-        },
-        NONE {
-            public SpacerType replace( SpacerType type ) {
-                return type == FIXED || type == DYNAMIC? type: this;
-            }
-
-        };
-        
-        public abstract SpacerType replace( SpacerType type );
-        
-        public Node create() {
-            return null;
-        }
-    }
 }

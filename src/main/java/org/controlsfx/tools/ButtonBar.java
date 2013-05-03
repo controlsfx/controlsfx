1c0256dc31df842232f3a4d56a4fac677763d8df
Jonathan Giles <jonathan@jonathangiles.net>
2013-05-03 17:37:47 +1200

    General cleanup of ButtonBar. Introduced OS-specific orderings, a ButtonType enum, and moved the buttonOrder from static to being a field.

--- src/main/java/org/controlsfx/tools/ButtonBar.java
+++ src/main/java/org/controlsfx/tools/ButtonBar.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.sun.javafx.Utils;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -15,41 +17,226 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 
 public final class ButtonBar extends HBox {
+    
+    /**************************************************************************
+     * 
+     * Static fields
+     * 
+     **************************************************************************/
 
     private static String CATEGORIZED_TYPES = "LRHEYNXBIACO";
+    
+    // represented as a ButtonType
     private static String BUTTON_TYPE_PROPERTY = "controlfx.button.type";
     
-    private static String order = "L_E+U+FBI_YNOCAH_R";
-
-    public static void setType( ButtonBase button, char type ) {
-        button.getProperties().put(BUTTON_TYPE_PROPERTY, String.valueOf(type));
+    /**
+     * The default button ordering on Windows.
+     */
+    public static final String BUTTON_ORDER_WINDOWS = "L_E+U+FBI_YNOCAH_R";
+    
+    /**
+     * The default button ordering on Mac OS.
+     */
+    public static final String BUTTON_ORDER_MAC_OS  = "L_HE+U+FBI_NYCOA_R";
+    
+    /**
+     * The default button ordering on Linux (specifically, GNOME).
+     */
+    public static final String BUTTON_ORDER_LINUX   = "L_HE+UNYACBXIO_R";
+    
+    
+    
+    /**************************************************************************
+     * 
+     * Static enumerations
+     * 
+     **************************************************************************/
+    
+    /**
+     * An enumeration of all available button types. By designating ever button
+     * in a {@link ButtonBar} as one of these types, the buttons will be 
+     * appropriately positioned relative to all other buttons in the ButtonBar.
+     */
+    public static enum ButtonType {
+        /**
+         * Buttons with this style tag will statically end up on the left end of the bar.
+         */
+        LEFT("L"),
+        
+        /**
+         * Buttons with this style tag will statically end up on the right end of the bar.
+         */
+        RIGHT("R"),
+        
+        /**
+         * A tag for the "help" button that normally is supposed to be on the right.
+         */
+        HELP("H"),
+        
+        /**
+         * A tag for the "help2" button that normally is supposed to be on the left.
+         */
+        HELP_2("E"),
+        
+        /**
+         * A tag for the "yes" button.
+         */
+        YES("Y"),
+        
+        /**
+         * A tag for the "no" button.
+         */
+        NO("N"),
+        
+        /**
+         * A tag for the "next >" or "forward >" button.
+         */
+        NEXT_FORWARD("X"),
+        
+        /**
+         * A tag for the "< back>" or "< previous" button.
+         */
+        BACK_PREVIOUS("B"),
+        
+        /**
+         * A tag for the "finish".
+         */
+        FINISH("I"),
+        
+        /**
+         * A tag for the "apply" button.
+         */
+        APPLY("A"),
+        
+        /**
+         * A tag for the "cancel" or "close" button.
+         */
+        CANCEL_CLOSE("C"),
+        
+        /**
+         * A tag for the "ok" or "done" button.
+         */
+        OK_DONE("O"),
+        
+        /**
+         * All Uncategorized, Other, or "Unknown" buttons. Tag will be "other".
+         */
+        OTHER("U"),
+        
+        /**
+         * A glue push gap that will take as much space as it can and at least 
+         * an "unrelated" gap. (Platform dependant)
+         */
+        BIG_GAP("+"),
+        
+        /**
+         * An "unrelated" gap. (Platform dependant)
+         */
+        SMALL_GAP("_");
+        
+        private final String type;
+        private ButtonType(String type) {
+            this.type = type;
+        }
+        
+        private String getType() {
+            return type;
+        }
     }
+    
+    /**
+     * Sets the given ButtonType on the given button. If this button is
+     * subsequently placed in a {@link ButtonBar} it will be placed in the 
+     * correct position relative to all other buttons in the bar.
+     * 
+     * @param button The button to tag with the given type.
+     * @param type The type to designate the button as.
+     */
+    public static void setType(ButtonBase button, ButtonType type) {
+        button.getProperties().put(BUTTON_TYPE_PROPERTY, type);
+    }
+    
+    
+    
+    /**************************************************************************
+     * 
+     * Private fields
+     * 
+     **************************************************************************/
+    
+    private String buttonOrder;
     
     private ObservableList<ButtonBase> buttons = FXCollections.<ButtonBase>observableArrayList();
     
     
+    
+    /**************************************************************************
+     * 
+     * Constructors
+     * 
+     **************************************************************************/
+    
     public ButtonBar() {
-     // TODO: the gap has to be OS dependent
+        // TODO: the gap has to be OS dependent
         super(10);
         buttons.addListener( new ListChangeListener<ButtonBase>() {
             @Override public void onChanged(ListChangeListener.Change<? extends ButtonBase> change) {
                 layoutButtons(buttons);
             }
         });
+        
+        // set the default button order 
+        if (Utils.isMac()) {
+            setButtonOrder(BUTTON_ORDER_MAC_OS);
+        } else if (Utils.isUnix()) {
+            setButtonOrder(BUTTON_ORDER_LINUX);
+        } else {
+            // windows by default
+            setButtonOrder(BUTTON_ORDER_WINDOWS);
+        }
     }
+    
+    
+    
+    /**************************************************************************
+     * 
+     * Public API
+     * 
+     **************************************************************************/
 
     public ObservableList<ButtonBase> getButtons() {
         return buttons;
     }
     
-
+    
+    /**
+     * Sets the order for the typical buttons in a standard button bar. It is 
+     * one letter per button type, and the applicable options are part of 
+     * {@link ButtonType}. Default button orders for operating systems are also 
+     * available: {@link #BUTTON_ORDER_WINDOWS}, {@link #BUTTON_ORDER_MAC_OS},
+     * {@link #BUTTON_ORDER_LINUX}.
+     */
+    public void setButtonOrder(String buttonOrder) {
+        this.buttonOrder = buttonOrder;
+        
+        layoutButtons(buttons);
+    }
+    
+    
+    
+    /**************************************************************************
+     * 
+     * Implementation
+     * 
+     **************************************************************************/
+    
     private void layoutButtons(List<? extends ButtonBase> buttons) {
         
         getChildren().clear();
         
         Map<String, List<ButtonBase>> buttonMap = buildButtonMap(buttons);
         SpacerType preparedSpacer = SpacerType.NONE;
-        for ( char type: order.toCharArray()) {
+        for ( char type: buttonOrder.toCharArray()) {
             
             if ( type == '+') {
                preparedSpacer = preparedSpacer.replace(SpacerType.DYNAMIC);
@@ -74,6 +261,38 @@ public final class ButtonBar extends HBox {
         }
         
     }
+    
+    private String getButtonType( ButtonBase btn ) {
+        ButtonType buttonType =  (ButtonType) btn.getProperties().get(BUTTON_TYPE_PROPERTY);
+        String typeString = buttonType.getType();
+        typeString = typeString.length() > 0? typeString.substring(0,1): "";
+        return CATEGORIZED_TYPES.contains(typeString.toUpperCase())? typeString: "U"; 
+    }
+    
+    private Map<String, List<ButtonBase>> buildButtonMap( List<? extends ButtonBase> buttons ) {
+        
+        Map<String, List<ButtonBase>> buttonMap = new HashMap<>();
+        for (ButtonBase btn : buttons) {
+            if ( btn == null ) continue;
+            String type =  getButtonType(btn); 
+            List<ButtonBase> typedButtons = buttonMap.get(type);
+            if ( typedButtons == null ) {
+                typedButtons = new ArrayList<ButtonBase>();
+                buttonMap.put(type, typedButtons);
+            }
+            typedButtons.add( btn );
+        }
+        return buttonMap;
+        
+    }
+
+    
+    
+    /**************************************************************************
+     * 
+     * Support classes / enums
+     * 
+     **************************************************************************/
     
     private enum SpacerType {
         FIXED {
@@ -113,28 +332,4 @@ public final class ButtonBar extends HBox {
             return null;
         }
     }
-    
-    private String getButtonType( ButtonBase btn ) {
-        String type =  (String) btn.getProperties().get(BUTTON_TYPE_PROPERTY);
-        type = type.length() > 0? type.substring(0,1): "";
-        return CATEGORIZED_TYPES.contains(type.toUpperCase())? type: "U"; 
-    }
-    
-    private Map<String, List<ButtonBase>> buildButtonMap( List<? extends ButtonBase> buttons ) {
-        
-        Map<String, List<ButtonBase>> buttonMap = new HashMap<>();
-        for (ButtonBase btn : buttons) {
-            if ( btn == null ) continue;
-            String type =  getButtonType(btn); 
-            List<ButtonBase> typedButtons = buttonMap.get(type);
-            if ( typedButtons == null ) {
-                typedButtons = new ArrayList<ButtonBase>();
-                buttonMap.put(type, typedButtons);
-            }
-            typedButtons.add( btn );
-        }
-        return buttonMap;
-        
-    }
-
 }
--- src/samples/java/org/controlsfx/samples/HelloButtonBar.java
+++ src/samples/java/org/controlsfx/samples/HelloButtonBar.java
@@ -3,14 +3,23 @@ package org.controlsfx.samples;
 import java.util.Arrays;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import org.controlsfx.tools.ButtonBar;
+import org.controlsfx.tools.ButtonBar.ButtonType;
+
+import static org.controlsfx.tools.ButtonBar.ButtonType.*;
 
 public class HelloButtonBar extends Application {
     
@@ -21,39 +30,64 @@ public class HelloButtonBar extends Application {
         BorderPane root = new BorderPane();
         root.setPadding(new Insets(10, 10, 10, 10));
         
-        ButtonBar bar = new ButtonBar();
-        bar.getButtons().addAll(Arrays.asList(
-                createButton("OK", 'O'),
-                createButton("Cancel", 'C'),
-                createButton("Left 1", 'L'),
-                createButton("Left 2", 'L'),
-                createButton("Left 3", 'L'),
-                createButton("Right 1", 'R'),
-                createButton("Unknown 1", '*'),
-                createButton("Help(R)", 'H'),
-                createButton("Help(L)", 'E'),
-                createButton("Unknown 2", '*'),
-                createButton("Yes", 'Y'),
-                createButton("No", 'N'),
-                createButton("Next", 'X'),
-                createButton("Unknown 3", '*'),
-                createButton("Back", 'B'),
-                createButton("Right 2", 'R'),
-                createButton("Finish", 'I'),
-                createButton("Right 3", 'R'),
-                createButton("Apply", 'A')
+        final ButtonBar buttonBar = new ButtonBar();
+        
+        
+        // create toggle button to switch button orders 
+        HBox buttonOrderHbox = new HBox(10);
+        ToggleGroup group = new ToggleGroup();
+        final ToggleButton windowsBtn = new ToggleButton("Windows");
+        final ToggleButton macBtn = new ToggleButton("Mac OS");
+        final ToggleButton linuxBtn = new ToggleButton("Linux");
+        buttonOrderHbox.getChildren().setAll(windowsBtn, macBtn, linuxBtn);
+        group.getToggles().setAll(windowsBtn, macBtn, linuxBtn);
+        windowsBtn.setSelected(true);
+        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                if (windowsBtn.equals(newValue)) {
+                    buttonBar.setButtonOrder(ButtonBar.BUTTON_ORDER_WINDOWS);
+                } else if (macBtn.equals(newValue)) {
+                    buttonBar.setButtonOrder(ButtonBar.BUTTON_ORDER_MAC_OS);
+                } else if (linuxBtn.equals(newValue)) {
+                    buttonBar.setButtonOrder(ButtonBar.BUTTON_ORDER_LINUX);
+                }
+            }
+        });
+        root.setTop(buttonOrderHbox);
+        
+        // create button bar
+        buttonBar.getButtons().addAll(Arrays.asList(
+                createButton("OK", OK_DONE),
+                createButton("Cancel", CANCEL_CLOSE),
+                createButton("Left 1", LEFT),
+                createButton("Left 2", LEFT),
+                createButton("Left 3", LEFT),
+                createButton("Right 1", RIGHT),
+                createButton("Unknown 1", OTHER),
+                createButton("Help(R)", HELP),
+                createButton("Help(L)", HELP_2),
+                createButton("Unknown 2", OTHER),
+                createButton("Yes", YES),
+                createButton("No", NO),
+                createButton("Next", NEXT_FORWARD),
+                createButton("Unknown 3", OTHER),
+                createButton("Back", BACK_PREVIOUS),
+                createButton("Right 2", RIGHT),
+                createButton("Finish", FINISH),
+                createButton("Right 3", RIGHT),
+                createButton("Apply", APPLY)
                 
         ));
-        root.setBottom( bar );
+        root.setBottom(buttonBar);
         
-        Scene scene = new Scene(root, 800, 300);
+        Scene scene = new Scene(root, 1300, 300);
         scene.setFill(Color.WHITE);
         
         stage.setScene(scene);
         stage.show();
     }
      
-    private Button createButton( String title, char type) {
+    private Button createButton( String title, ButtonType type) {
         Button button = new Button(title);
         ButtonBar.setType(button, type);
         return button;

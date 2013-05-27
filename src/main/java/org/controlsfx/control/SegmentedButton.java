1c867656c6b2fb86fc7b1600d495ae9002e0a5b2
Jonathan Giles <jonathan@jonathangiles.net>
2013-05-27 18:51:46 +1200

    Improved javadoc (and sample) for SegmentedButton control. It is now complete in my opinion.

--- src/main/java/org/controlsfx/control/SegmentedButton.java
+++ src/main/java/org/controlsfx/control/SegmentedButton.java
@@ -32,7 +32,43 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.Control;
 import javafx.scene.control.Skin;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
+/**
+ * The SegmentedButton is a simple control that forces together a group of
+ * {@link ToggleButton} instances such that they appear as one collective button
+ * (with sub-buttons), rather than as individual buttons. This is better 
+ * clarified with a picture:
+ * 
+ * <br/>
+ * <center>
+ * <img src="segmentedButton.png"/>
+ * </center>
+ * 
+ * <p>There is very little API on this control, you essentially create
+ * {@link ToggleButton} instances as per usual (and don't bother putting them
+ * into a {@link ToggleGroup}, as this is done by the SegmentedButton itself), and then 
+ * you place these buttons inside the {@link #getButtons() buttons list}. The 
+ * long-hand way to code this is as follows:
+ * 
+ * <pre>
+ * {@code 
+ * ToggleButton b1 = new ToggleButton("day");
+ * ToggleButton b2 = new ToggleButton("week");
+ * ToggleButton b3 = new ToggleButton("month");
+ * ToggleButton b4 = new ToggleButton("year");
+ *       
+ * SegmentedButton segmentedButton = new SegmentedButton();    
+ * segmentedButton.getButtons().addAll(b1, b2, b3, b4);}</pre>
+ * 
+ * <p>A slightly shorter way of doing this is to pass the ToggleButton instances
+ * into the varargs constructor, as such:
+ * 
+ * <pre>{@code SegmentedButton segmentedButton = new SegmentedButton(b1, b2, b3, b4);}</pre>
+ * 
+ * @see ToggleButton
+ * @see ToggleGroup
+ */
 public class SegmentedButton extends Control {
     
     private final ObservableList<ToggleButton> buttons;
@@ -44,15 +80,30 @@ public class SegmentedButton extends Control {
      *************************************************************************/
     
     /**
-     * 
+     * Creates a default SegmentedButton instance with no buttons.
      */
     public SegmentedButton() {
-        this(null);
+        this((ObservableList<ToggleButton>)null);
     }
     
     /**
+     * Creates a default SegmentedButton instance with the provided buttons
+     * inserted into it.
      * 
-     * @param buttons
+     * @param buttons A varargs array of buttons to add into the SegmentedButton
+     *      instance.
+     */
+    public SegmentedButton(ToggleButton... buttons) {
+        this(buttons == null ? 
+                FXCollections.<ToggleButton>observableArrayList() : 
+                FXCollections.observableArrayList(buttons));
+    }
+    
+    /**
+     * Creates a default SegmentedButton instance with the provided buttons
+     * inserted into it.
+     * 
+     * @param buttons A list of buttons to add into the SegmentedButton instance.
      */
     public SegmentedButton(ObservableList<ToggleButton> buttons) {
         getStyleClass().add("segmented-button");
@@ -75,8 +126,10 @@ public class SegmentedButton extends Control {
     }
     
     /**
-     * 
-     * @return
+     * Returns the list of buttons that this SegmentedButton will draw together
+     * into one 'grouped button'. It is possible to modify this list to add or
+     * remove {@link ToggleButton} instances, as shown in the javadoc 
+     * documentation for this class.
      */
     public final ObservableList<ToggleButton> getButtons() {
         return buttons;
--- src/samples/java/org/controlsfx/samples/HelloSegmentedButton.java
+++ src/samples/java/org/controlsfx/samples/HelloSegmentedButton.java
@@ -6,8 +6,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import org.controlsfx.Sample;
@@ -24,20 +27,35 @@ public class HelloSegmentedButton extends Application implements Sample {
     }
     
     @Override public Node getPanel(Stage stage) {
-        ToggleButton b1 = new ToggleButton("day");
-        ToggleButton b2 = new ToggleButton("week");
-        ToggleButton b3 = new ToggleButton("month");
-        ToggleButton b4 = new ToggleButton("year");
+        GridPane grid = new GridPane();
+        grid.setVgap(10);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(30, 30, 0, 30));
         
-        SegmentedButton pillBox = new SegmentedButton();    
-        pillBox.getButtons().addAll(b1, b2, b3, b4);
-        Button b = new Button("Submit");
+        // without segmented button
+        grid.add(new Label("Without SegmentedButton (with 10px spacing): "), 0, 0);
+        
+        ToggleButton without_b1 = new ToggleButton("day");
+        ToggleButton without_b2 = new ToggleButton("week");
+        ToggleButton without_b3 = new ToggleButton("month");
+        ToggleButton without_b4 = new ToggleButton("year");
+        
+        HBox toggleButtons = new HBox(without_b1, without_b2, without_b3, without_b4);
+        toggleButtons.setSpacing(10);
+        grid.add(toggleButtons, 1, 0);
+        
+        // with segmented button
+        grid.add(new Label("With SegmentedButton: "), 0, 1);
+        
+        ToggleButton with_b1 = new ToggleButton("day");
+        ToggleButton with_b2 = new ToggleButton("week");
+        ToggleButton with_b3 = new ToggleButton("month");
+        ToggleButton with_b4 = new ToggleButton("year");
+        
+        SegmentedButton segmentedButton = new SegmentedButton(with_b1, with_b2, with_b3, with_b4);    
+        grid.add(segmentedButton, 1, 1);
 
-        HBox container = new HBox(20);
-        container.setPadding(new Insets(30, 30, 0, 30));
-        container.getChildren().addAll(pillBox);
-        
-        return container;
+        return grid;
     }
     
     @Override public void start(Stage stage) throws Exception {

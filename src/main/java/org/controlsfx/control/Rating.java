c9c521564cc8d711fcb9a7f1538c40e4ee486c42
Jonathan Giles <jonathan@jonathangiles.net>
2013-05-27 18:29:04 +1200

    Improved javadoc for the Rating control.

--- src/main/java/org/controlsfx/control/Rating.java
+++ src/main/java/org/controlsfx/control/Rating.java
@@ -36,7 +36,78 @@ import javafx.scene.control.Skin;
  * A control for allowing users to provide a rating. This control supports
  * {@link #partialRatingProperty() partial ratings} (i.e. not whole numbers and
  * dependent upon where the user clicks in the control) and 
- * {@link #updateOnHoverProperty() updating the rating on hover}.
+ * {@link #updateOnHoverProperty() updating the rating on hover}. Read on for
+ * more examples!
+ * 
+ * <h3>Examples</h3>
+ * It can be hard to appreciate some of the features of the Rating control, so
+ * hopefully the following screenshots will help. Firstly, here is what the 
+ * standard (horizontal) Rating control looks like when it has five stars, and a 
+ * rating of two:
+ * 
+ * <br/>
+ * <center>
+ * <img src="rating-horizontal.png"/>
+ * </center>
+ * 
+ * <p>To create a Rating control that looks like this is simple:
+ * 
+ * <pre>
+ * {@code 
+ * final Rating rating = new Rating();}</pre>
+ * 
+ * <p>This creates a default horizontal rating control. To create a vertical 
+ * Rating control, simply change the orientation, as such:
+ * 
+ * <pre>
+ * {@code 
+ * final Rating rating = new Rating();
+ * rating.setOrientation(Orientation.VERTICAL);}</pre>
+ * 
+ * <p>The end result of making this one line change is shown in the screenshot
+ * below:
+ * 
+ * <br/>
+ * <center>
+ * <img src="rating-vertical.png"/>
+ * </center>
+ * 
+ * <p>One of the features of the Rating control is that it doesn't just allow
+ * for 'integer' ratings: it also allows for the user to click anywhere within
+ * the rating area to set a 'float' rating. This is hard to describe, but easy 
+ * to show in a picture:
+ * 
+ * <br/>
+ * <center>
+ * <img src="rating-partial.png"/>
+ * </center>
+ * 
+ * <p>In essence, in the screenshot above, the user clicked roughly in the 
+ * middle of the third star. This results in a rating of approximately 2.44.
+ * To enable {@link #partialRatingProperty() partial ratings}, simply do the
+ * following when instantiating the Rating control:
+ * 
+ * <pre>
+ * {@code 
+ * final Rating rating = new Rating();
+ * rating.setPartialRating(true);}</pre>
+ * 
+ * <p>So far all of the Rating controls demonstrated above have
+ * required the user to click on the stars to register their rating. This may not
+ * be the preferred user interaction - often times the preferred approach is to
+ * simply allow for the rating to be registered by the user hovering their mouse
+ * over the rating stars. This mode is also supported by the Rating control,
+ * using the {@link #updateOnHoverProperty() update on hover} property, as such:
+ * 
+ * <pre>
+ * {@code 
+ * final Rating rating = new Rating();
+ * rating.setUpdateOnHover(true);}</pre>
+ * 
+ * <p>It is also allowable to have a Rating control that both updates on hover
+ * and allows for partial values: the 'golden fill' of the default graphics will
+ * automatically follow the users mouse as they move it along the Rating scale.
+ * To enable this, just set both properties to true.
  */
 public class Rating extends Control {
     

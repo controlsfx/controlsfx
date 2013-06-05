49a1e792fe24f5aafb4d476ca1afb17c47eb0ef5
Jonathan Giles <jonathan@jonathangiles.net>
2013-06-05 17:36:30 +1200

    Fixing up WorkerProgressPane sample.

--- src/samples/java/org/controlsfx/samples/HelloWorkerProgressPane.java
+++ src/samples/java/org/controlsfx/samples/HelloWorkerProgressPane.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import org.controlsfx.Sample;
 import org.controlsfx.control.WorkerProgressPane;
 
 /**
@@ -16,18 +20,27 @@ import org.controlsfx.control.WorkerProgressPane;
  * Date: 6/3/13
  * Time: 10:49 AM
  */
-public class HelloWorkerProgressPane extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+public class HelloWorkerProgressPane extends Application implements Sample {
+    
+    @Override public String getSampleName() {
+        return "Worker Progress Pane";
+    }
+    
+    @Override public String getJavaDocURL() {
+        return Utils.JAVADOC_BASE + "org/controlsfx/control/WorkerProgressPane.html";
+    }
+    
+    
+    @Override public Node getPanel(Stage stage) {
         StackPane root = new StackPane();
         Button b = new Button("Press Me");
         final WorkerProgressPane pane = new WorkerProgressPane();
         root.getChildren().addAll(b, pane);
 
-        Scene scene = new Scene(root, 640, 480);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Hello WorkerProgressPane");
-        primaryStage.show();
+//        Scene scene = new Scene(root, 640, 480);
+//        stage.setScene(scene);
+//        stage.setTitle("Hello WorkerProgressPane");
+//        stage.show();
 
         b.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
@@ -48,6 +61,19 @@ public class HelloWorkerProgressPane extends Application {
                 System.out.println("Started");
             }
         });
+        
+        return root;
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello WorkerProgressPane");
+        
+        Scene scene = new Scene((Parent)getPanel(stage), 1300, 300);
+        scene.setFill(Color.WHITE);
+        
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {

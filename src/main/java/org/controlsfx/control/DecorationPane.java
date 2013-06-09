f4eca6a95626985b4acb145e2264ef75eda0b7fc
Eugene Ryzhikov <eugene.ryzhikov@gmail.com>
2013-06-08 23:42:36 -0400

    Add getChildren to DecorationPane

--- src/main/java/org/controlsfx/control/DecorationPane.java
+++ src/main/java/org/controlsfx/control/DecorationPane.java
@@ -1,15 +1,16 @@
 package org.controlsfx.control;
 
 import impl.org.controlsfx.skin.DecorationPaneSkin;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
 import javafx.scene.control.Skin;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.Pane;
 
 public class DecorationPane extends Control {
 	
 	private final Node node;
-	private final Region overlay = new Region();
+	private final Pane overlay = new Pane();
 
 	public DecorationPane( Node node ) {
 		this.node = node;
@@ -22,4 +23,8 @@ public class DecorationPane extends Control {
     public Node getNode() {
 		return node;
 	}
+    
+    public final ObservableList<Node> getChildren() {
+    	return overlay.getChildren();
+    }
 }

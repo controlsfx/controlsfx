34ee5b49c572334fc5816d402fb2b1dd61ea4f0d
Eugene Ryzhikov <eugene.ryzhikov@gmail.com>
2013-06-12 23:43:28 -0400

    Convert tabs to spaces; add licenses

--- src/main/java/org/controlsfx/decoration/DefaultDecoration.java
+++ src/main/java/org/controlsfx/decoration/DefaultDecoration.java
@@ -1,30 +1,54 @@
+/**
+ * Copyright (c) 2013, ControlsFX
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ControlsFX, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CONTROLSFX BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.controlsfx.decoration;
 
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 
 public class DefaultDecoration implements Decoration {
-	
-	private final Node node;
-	private final Pos pos;
 
-	public DefaultDecoration( Node decoration, Pos position ) {
-		this.node = decoration;
-		this.pos = position;
-	}
-	
-	public DefaultDecoration( Node decoration ) {
-		this( decoration, Pos.TOP_LEFT);
-	}
+    private final Node node;
+    private final Pos pos;
 
-	@Override
-	public Node getNode() {
-		return node;
-	}
-	
-	@Override
-	public Pos getPosition() {
-		return pos;
-	}
-	
+    public DefaultDecoration(Node decoration, Pos position) {
+        this.node = decoration;
+        this.pos = position;
+    }
+
+    public DefaultDecoration(Node decoration) {
+        this(decoration, Pos.TOP_LEFT);
+    }
+
+    @Override public Node getNode() {
+        return node;
+    }
+
+    @Override public Pos getPosition() {
+        return pos;
+    }
+
 }

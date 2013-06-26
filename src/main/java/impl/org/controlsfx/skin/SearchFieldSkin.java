5edb4b46ba98dabba829a4b01f8fc24ad23c42bb
Jonathan Giles <jonathan@jonathangiles.net>
2013-06-26 15:40:05 +1200

    Quick patch on SearchField to allow for proper scrolling of text (so it doesn't go under the clear button). A bit hacky, but it'll do for now...

--- src/main/java/impl/org/controlsfx/skin/SearchFieldSkin.java
+++ src/main/java/impl/org/controlsfx/skin/SearchFieldSkin.java
@@ -3,6 +3,7 @@ package impl.org.controlsfx.skin;
 import javafx.animation.FadeTransition;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.css.PseudoClass;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
@@ -21,23 +22,31 @@ public class SearchFieldSkin extends BehaviorSkinBase<SearchField, BehaviorBase<
     private static final Duration FADE_DURATION = Duration.millis(350);
     private final FadeTransition fader;
     
+    private static final PseudoClass HAS_FOCUS = PseudoClass.getPseudoClass("text-field-has-focus");
+    
     private final TextField textField;
     
     private boolean clearButtonShowing = false;
     private final StackPane clearButtonPane;
     private final Region clearButton;
     
-    public SearchFieldSkin(SearchField control) {
+    public SearchFieldSkin(final SearchField control) {
         super(control, new BehaviorBase<>(control));
         
         textField = control.getTextField();
+        textField.getStyleClass().clear();
+        textField.focusedProperty().addListener(new InvalidationListener() {
+            @Override public void invalidated(Observable arg0) {
+                control.pseudoClassStateChanged(HAS_FOCUS, textField.isFocused());
+            }
+        });
         
         clearButton = new Region();
         clearButton.getStyleClass().addAll("graphic");
         clearButtonPane = new StackPane(clearButton);
         clearButtonPane.getStyleClass().addAll("clear-button");
         clearButtonPane.setOpacity(0.0);
-        Tooltip.install(clearButtonPane, new Tooltip("Clear the search text"));
+//        Tooltip.install(clearButtonPane, new Tooltip("Clear the search text"));
         
         clearButtonPane.setOnMouseReleased(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent e) {
@@ -76,11 +85,12 @@ public class SearchFieldSkin extends BehaviorSkinBase<SearchField, BehaviorBase<
     }
     
     @Override protected void layoutChildren(double x, double y, double w, double h) {
-        textField.resizeRelocate(x, y, w, h);
+        double btnWidth = clearButtonPane.prefWidth(h);
+        double btnHeight = clearButtonPane.prefHeight(w);
+        final double btnStartX = w - snappedRightInset() - btnWidth;
         
-        double btnWidth = clearButton.prefWidth(h);
-        double btnHeight = clearButton.prefHeight(w);
-        clearButtonPane.resizeRelocate(w - snappedRightInset() - btnWidth - 8, h / 2.0 - btnHeight / 2.0, btnWidth, btnHeight);
+        textField.resizeRelocate(x, y, btnStartX - 11, h);
+        clearButtonPane.resizeRelocate(btnStartX, h / 2.0 - btnHeight / 2.0, btnWidth, btnHeight);
     }
     
     @Override

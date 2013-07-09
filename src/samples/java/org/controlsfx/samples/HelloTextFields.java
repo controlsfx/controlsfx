3bdc1b877df2c16a461efd314ed84baccc453262
Jonathan Giles <jonathan@jonathangiles.net>
2013-07-09 16:34:21 +1200

    Change HelloCustomTextField to HelloTextFields

--- src/samples/java/org/controlsfx/samples/HelloTextFields.java
+++ src/samples/java/org/controlsfx/samples/HelloTextFields.java
@@ -16,16 +16,16 @@ import org.controlsfx.Sample;
 import org.controlsfx.control.CustomTextField;
 import org.controlsfx.control.TextFields;
 
-public class HelloCustomTextField extends Application implements Sample {
+public class HelloTextFields extends Application implements Sample {
     
     private static final Image image = new Image("/org/controlsfx/samples/security-low.png");
     
     @Override public String getSampleName() {
-        return "CustomTextField";
+        return "TextFields";
     }
     
     @Override public String getJavaDocURL() {
-        return Utils.JAVADOC_BASE + "org/controlsfx/control/CustomTextField.html";
+        return Utils.JAVADOC_BASE + "org/controlsfx/control/TextFields.html";
     }
     
     @Override public boolean includeInSamples() {
@@ -37,38 +37,40 @@ public class HelloCustomTextField extends Application implements Sample {
         grid.setVgap(10);
         grid.setHgap(10);
         grid.setPadding(new Insets(30, 30, 0, 30));
+        
+        int row = 0;
 
         // normal TextField
-        grid.add(new Label("Normal TextField: "), 0, 0);
-        grid.add(new TextField(), 1, 0);
+        grid.add(new Label("Normal TextField: "), 0, row);
+        grid.add(new TextField(), 1, row++);
         
         // SearchField
-        grid.add(new Label("SearchField: "), 0, 1);
-        grid.add(TextFields.createSearchField(), 1, 1);
+        grid.add(new Label("SearchField: "), 0, row);
+        grid.add(TextFields.createSearchField(), 1, row++);
         
         // CustomTextField
-        grid.add(new Label("CustomTextField (no additional nodes): "), 0, 2);
-        grid.add(new CustomTextField(), 1, 2);
+        grid.add(new Label("CustomTextField (no additional nodes): "), 0, row);
+        grid.add(new CustomTextField(), 1, row++);
         
         // CustomTextField (w/ right node)
-        grid.add(new Label("CustomTextField (w/ right node): "), 0, 3);
+        grid.add(new Label("CustomTextField (w/ right node): "), 0, row);
         CustomTextField customTextField1 = new CustomTextField();
         customTextField1.setRight(new ImageView(image));
-        grid.add(customTextField1, 1, 3);
+        grid.add(customTextField1, 1, row++);
         
         // CustomTextField (w/ left node)
-        grid.add(new Label("CustomTextField (w/ left node): "), 0, 4);
+        grid.add(new Label("CustomTextField (w/ left node): "), 0, row);
         CustomTextField customTextField2 = new CustomTextField();
         customTextField2.setLeft(new ImageView(image));
-        grid.add(customTextField2, 1, 4);
+        grid.add(customTextField2, 1, row++);
         
         // CustomTextField (w/ left + right node)
-        grid.add(new Label("CustomTextField (w/ left + right node): "), 0, 5);
+        grid.add(new Label("CustomTextField (w/ left + right node): "), 0, row);
         CustomTextField customTextField3 = new CustomTextField();
         ImageView imageView = new ImageView(image);
         customTextField3.setLeft(imageView);
         customTextField3.setRight(new ImageView(image));
-        grid.add(customTextField3, 1, 5);
+        grid.add(customTextField3, 1, row++);
         
         return grid;
     }

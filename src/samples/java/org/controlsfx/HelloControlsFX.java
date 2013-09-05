6ee298a4c48914be29e7412e40a3d69a47aff4b3
Jonathan Giles <jonathan@jonathangiles.net>
2013-09-06 07:49:15 +1200

    Added support to HelloControlsFX sample app to be able to find samples within jar files, so that the samples.jar file works (which I don't think it ever has up until this point - hence all the emails).

--- src/samples/java/org/controlsfx/HelloControlsFX.java
+++ src/samples/java/org/controlsfx/HelloControlsFX.java
@@ -27,6 +27,7 @@
 package org.controlsfx;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -36,6 +37,8 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
@@ -63,37 +66,37 @@ import javafx.util.Callback;
 import org.controlsfx.control.HyperlinkLabel;
 
 public class HelloControlsFX extends Application {
-    
+
     private static final String SAMPLES_ROOT_PACKAGE = "org.controlsfx.samples";
 
     private final Map<String, TreeItem<Sample>> packageTreeItemMap = new HashMap<>();
-    
+
     private GridPane grid;
 
     private TabPane tabPane;
     private Tab welcomeTab;
     private Tab sampleTab;
     private Tab webViewTab;
-    
+
     private WebView webview;
-    
-    
+
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     @Override public void start(final Stage primaryStage) throws Exception {
         setUserAgentStylesheet(STYLESHEET_MODENA);
-        
+
         primaryStage.getIcons().add(new Image("/org/controlsfx/samples/controlsfx-logo.png"));
-        
+
         final TreeItem<Sample> root = new TreeItem<Sample>(new EmptySample("ControlsFX"));
         root.setExpanded(true);
-        
+
         Class<?>[] sampleClasses = getClasses(SAMPLES_ROOT_PACKAGE);
         for (Class<?> sampleClass : sampleClasses) {
             if (! Sample.class.isAssignableFrom(sampleClass)) continue;
-            
+
             final Sample sample = (Sample)sampleClass.newInstance();
             if (! sample.includeInSamples()) continue;
 
@@ -101,12 +104,12 @@ public class HelloControlsFX extends Application {
             String displayName = packageName.substring(packageName.lastIndexOf(".") + 1);
             displayName = displayName.substring(0, 1).toUpperCase() + displayName.substring(1);
             displayName = displayName.replace("_", " ");
-            
+
             if (SAMPLES_ROOT_PACKAGE.equals(packageName)) {
                 root.getChildren().add(new TreeItem<Sample>(sample));
                 continue;
             }
-            
+
             TreeItem<Sample> packageTreeItem;
             if (packageTreeItemMap.containsKey(displayName)) {
                 packageTreeItem = packageTreeItemMap.get(displayName);
@@ -114,28 +117,28 @@ public class HelloControlsFX extends Application {
                 packageTreeItem = new TreeItem<Sample>(new EmptySample(displayName));
                 packageTreeItemMap.put(displayName, packageTreeItem);
             }
-            
+
             // now that we have the package TreeItem, we create a child TreeItem
             // for the actual sample
             TreeItem<Sample> sampleTreeItem = new TreeItem<Sample>(sample);
             packageTreeItem.getChildren().add(sampleTreeItem);
             packageTreeItem.setExpanded(true);
         }
-        
+
         root.getChildren().addAll(packageTreeItemMap.values());
-        
+
         Collections.sort(root.getChildren(), new Comparator<TreeItem<Sample>>() {
             @Override public int compare(TreeItem<Sample> o1, TreeItem<Sample> o2) {
                 return o1.getValue().getSampleName().compareTo(o2.getValue().getSampleName());
             }
         });
-        
+
         // simple layout: ListView on left, sample area on right
         grid = new GridPane();
         grid.setPadding(new Insets(5, 10, 10, 10));
         grid.setHgap(10);
         grid.setVgap(10);
-        
+
         // --- left hand side
         TreeView<Sample> samplesTreeView = new TreeView<>(root);
         samplesTreeView.setMinWidth(200);
@@ -145,7 +148,7 @@ public class HelloControlsFX extends Application {
                 return new TreeCell<Sample>() {
                     @Override protected void updateItem(Sample item, boolean empty) {
                         super.updateItem(item, empty);
-                        
+
                         if (empty) {
                             setText("");
                         } else {
@@ -169,7 +172,7 @@ public class HelloControlsFX extends Application {
         GridPane.setVgrow(samplesTreeView, Priority.ALWAYS);
         GridPane.setMargin(samplesTreeView, new Insets(5, 0, 0, 0));
         grid.add(samplesTreeView, 0, 0);
-        
+
         // right hand side
         tabPane = new TabPane();
         tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
@@ -177,15 +180,15 @@ public class HelloControlsFX extends Application {
         GridPane.setHgrow(tabPane, Priority.ALWAYS);
         GridPane.setVgrow(tabPane, Priority.ALWAYS);
         grid.add(tabPane, 1, 0, 1, 1);
-        
+
         sampleTab = new Tab("Sample");
         webViewTab = new Tab("JavaDoc");
         webview = new WebView();
-    	webViewTab.setContent(webview);
-        
+        webViewTab.setContent(webview);
+
         // by default we'll have a welcome message in the right-hand side
         changeToWelcomeTab();
-        
+
         // put it all together
         Scene scene = new Scene(grid);
         primaryStage.setScene(scene);
@@ -195,7 +198,7 @@ public class HelloControlsFX extends Application {
         primaryStage.setHeight(600);
         primaryStage.setTitle("ControlsFX!");
         primaryStage.show();
-        
+
         samplesTreeView.requestFocus();
     }
 
@@ -203,64 +206,64 @@ public class HelloControlsFX extends Application {
         if (newSample == null) {
             return;
         }
-        
-    	if (tabPane.getTabs().contains(welcomeTab)) {
-    		tabPane.getTabs().setAll(sampleTab, webViewTab);
-    	}
-    	
-    	// update the sample tab
-    	sampleTab.setContent(newSample.getPanel(stage));
-    	
-    	// update the javadoc tab
-    	webview.getEngine().load(newSample.getJavaDocURL());
+
+        if (tabPane.getTabs().contains(welcomeTab)) {
+            tabPane.getTabs().setAll(sampleTab, webViewTab);
+        }
+
+        // update the sample tab
+        sampleTab.setContent(newSample.getPanel(stage));
+
+        // update the javadoc tab
+        webview.getEngine().load(newSample.getJavaDocURL());
     }
-    
+
     private void changeToWelcomeTab() {
         // line 1
         Label welcomeLabel1 = new Label("Welcome to ControlsFX!");
         welcomeLabel1.setStyle("-fx-font-size: 2em; -fx-padding: 0 0 0 5;");
-        
+
         // line 2
         HyperlinkLabel welcomeLabel2 = new HyperlinkLabel(
                 "Explore the available UI controls by clicking on the options to the left.\n\n" +
-                "There have been many contributors to this project, including:\n" +
-                "   Jonathan Giles\n" +
-                "   Eugene Ryzhikov\n" +
-                "   Hendrik Ebbers\n" +
-                "   Danno Ferrin\n" +
-                "   Paru Somashekar\n\n" +
-                "If you ever meet any of these wonderful contributors, tell them how great they are! :-)\n\n" +
+                        "There have been many contributors to this project, including:\n" +
+                        "   Jonathan Giles\n" +
+                        "   Eugene Ryzhikov\n" +
+                        "   Hendrik Ebbers\n" +
+                        "   Danno Ferrin\n" +
+                        "   Paru Somashekar\n\n" +
+                        "If you ever meet any of these wonderful contributors, tell them how great they are! :-)\n\n" +
                 "To keep up to date with the ControlsFX project, visit the website at [http://www.controlsfx.org]");
         welcomeLabel2.setStyle("-fx-font-size: 1.25em; -fx-padding: 0 0 0 5;");
         welcomeLabel2.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent event) {
                 webViewTab.setText("ControlsFX Website");
-                
+
                 webview.getEngine().load("http://www.controlsfx.org");
                 tabPane.getTabs().add(webViewTab);
                 tabPane.getSelectionModel().select(webViewTab);
             }
         });
-        
+
         VBox initialVBox = new VBox(5, welcomeLabel1, welcomeLabel2);
-        
+
         welcomeTab = new Tab("Welcome to ControlsFX!");
         welcomeTab.setContent(initialVBox);
-        
+
         tabPane.getTabs().setAll(welcomeTab);
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     private static class EmptySample implements Sample {
         private final String name;
-        
+
         public EmptySample(String name) {
             this.name = name;
         }
-        
+
         @Override public String getSampleName() {
             return name;
         }
@@ -272,15 +275,15 @@ public class HelloControlsFX extends Application {
         @Override public String getJavaDocURL() {
             return null;
         }
-        
+
         @Override public boolean includeInSamples() {
             return true;
         }
     }
-    
-    
-    
-    
+
+
+
+
     // --- Following code taken from http://dzone.com/snippets/get-all-classes-within-package
     /**
      * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
@@ -292,8 +295,8 @@ public class HelloControlsFX extends Application {
      */
     private static Class<?>[] getClasses(String packageName) throws ClassNotFoundException, IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
         String path = packageName.replace('.', '/');
+        
         Enumeration<URL> resources = classLoader.getResources(path);
         List<File> dirs = new ArrayList<File>();
         while (resources.hasMoreElements()) {
@@ -302,7 +305,18 @@ public class HelloControlsFX extends Application {
         }
         ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
         for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
+            String fullPath = directory.getAbsolutePath();
+            
+            if (fullPath.contains(".jar")) {
+                // scan the jar, but first we need to strip out everything after
+                // the .jar text (and before the file:)
+                fullPath = fullPath.substring(fullPath.indexOf("file:") + 6);
+                fullPath = fullPath.substring(0, fullPath.indexOf(".jar") + 4);
+                classes.addAll(findClassesInJar(new File(fullPath), packageName));
+            } else {
+                // scan the classpath
+                classes.addAll(findClasses(directory, packageName));
+            }
         }
         return classes.toArray(new Class[classes.size()]);
     }
@@ -318,15 +332,52 @@ public class HelloControlsFX extends Application {
     private static List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
         List<Class<?>> classes = new ArrayList<Class<?>>();
         if (!directory.exists()) {
+            System.out.println("Directory does not exist: " + directory.getAbsolutePath());
             return classes;
         }
+        
         File[] files = directory.listFiles();
         for (File file : files) {
             if (file.isDirectory()) {
                 assert !file.getName().contains(".");
                 classes.addAll(findClasses(file, packageName + "." + file.getName()));
             } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                classes.add(Class.forName(className));
+            }
+        }
+        return classes;
+    }
+
+
+    private static List<Class<?>> findClassesInJar(File jarFile, String packageName) throws IOException, ClassNotFoundException {
+        List<Class<?>> classes = new ArrayList<Class<?>>();
+        if (!jarFile.exists()) {
+            System.out.println("Jar file does not exist here: " + jarFile.getAbsolutePath());
+            return classes;
+        }
+        
+        try (ZipInputStream zip = new ZipInputStream(new FileInputStream(jarFile))) {
+            for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
+                if (entry.getName().endsWith(".class") && !entry.isDirectory()) {
+                    
+                    StringBuilder className = new StringBuilder();
+                    for (String part : entry.getName().split("/")) {
+                        if(className.length() != 0) {
+                            className.append(".");
+                        }
+                        
+                        className.append(part);
+                        
+                        if(part.endsWith(".class")) {
+                            className.setLength(className.length()-".class".length());
+                        }
+                    }
+                    
+                    if (className.toString().contains(packageName)) {
+                        classes.add(Class.forName(className.toString()));
+                    }
+                }
             }
         }
         return classes;

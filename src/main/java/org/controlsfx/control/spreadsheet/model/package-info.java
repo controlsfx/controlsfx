bf4329189d7e00fbf857b1c67951437bd4d73554
Jonathan Giles <jonathan@jonathangiles.net>
2013-09-21 10:50:16 +1200

    Add in package-info.java files for the two SpreadsheetView packages.

---
+++ src/main/java/org/controlsfx/control/spreadsheet/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A package containing the model-related API for the 
+ * {@link org.controlsfx.control.SpreadsheetView} control.
+ */
+package org.controlsfx.control.spreadsheet.model;
---
+++ src/main/java/org/controlsfx/control/spreadsheet/view/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A package containing the view-related API for the 
+ * {@link org.controlsfx.control.SpreadsheetView} control.
+ */
+package org.controlsfx.control.spreadsheet.view;

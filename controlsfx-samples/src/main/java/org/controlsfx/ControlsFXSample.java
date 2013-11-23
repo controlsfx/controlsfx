98f10ac141fb64c85255e9b69bb45e2c6021750f
Eugene Ryzhikov <eugene.ryzhikov@gmail.com>
2013-11-23 17:13:54 -0500

    Show ControlsFX version in FXSampler. Based on resource filtering in manifest.mf file committed earlier

--- controlsfx-samples/src/main/java/org/controlsfx/ControlsFXSample.java
+++ controlsfx-samples/src/main/java/org/controlsfx/ControlsFXSample.java
@@ -1,10 +1,49 @@
 package org.controlsfx;
 
+import java.io.InputStream;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 import fxsampler.SampleBase;
 
 public abstract class ControlsFXSample extends SampleBase {
 
-    @Override public String getProjectName() {
-        return "ControlsFX";
-    }
+	private static final ProjectInfo projectInfo = new ProjectInfo();
+	
+	@Override
+	public String getProjectName() {
+		return "ControlsFX";
+	}
+
+	@Override
+	public String getProjectVersion() {
+		return projectInfo.getVersion();
+	}
+	
+	private static class ProjectInfo {
+		
+		private String version;
+		
+		
+		public ProjectInfo() {
+			
+			InputStream s = getClass().getClassLoader().getResourceAsStream(
+					"META-INF/manifest.mf");
+			
+			try {
+				Manifest manifest = new Manifest(s);
+				Attributes attr = manifest.getMainAttributes();
+				version = attr.getValue("Implementation-Version");
+			} catch (Throwable e) {
+				e.printStackTrace();
+				version = "???";
+			}
+		}
+		
+		public String getVersion() {
+			return version;
+		}
+	}
+
 }
+

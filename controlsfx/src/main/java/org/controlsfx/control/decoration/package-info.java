9ed98bb6f4330316612c7ae74156706549cd6262
Jonathan Giles <jonathan@jonathangiles.net>
2014-05-29 16:39:16 +1200

    JavaDoc improvements

---
+++ controlsfx/src/main/java/org/controlsfx/control/decoration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A package containing decoration-related API (that is, API to allow for users
+ * to 'decorate' nodes with additional nodes or css decorations.
+ */
+package org.controlsfx.control.decoration;

e322855acc85b2d53d90f68249de233f917ba3f5
Jonathan Giles <jonathan@jonathangiles.net>
2014-09-14 13:39:24 +1200

    Fix for issue #336: Blinking validation popup

--- controlsfx/src/main/java/org/controlsfx/validation/decoration/GraphicValidationDecoration.java
+++ controlsfx/src/main/java/org/controlsfx/validation/decoration/GraphicValidationDecoration.java
@@ -63,12 +63,13 @@ public class GraphicValidationDecoration extends AbstractValidationDecoration {
     private static final Image REQUIRED_IMAGE = new Image("/impl/org/controlsfx/control/validation/required-indicator.png"); //$NON-NLS-1$
 
     private static final String SHADOW_EFFECT = "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"; //$NON-NLS-1$
+    private static final String POPUP_SHADOW_EFFECT = "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 5, 0, 0, 5);"; //$NON-NLS-1$
     private static final String TOOLTIP_COMMON_EFFECTS = "-fx-font-weight: bold; -fx-padding: 5; -fx-border-width:1;"; //$NON-NLS-1$
     
-    private static final String ERROR_TOOLTIP_EFFECT = SHADOW_EFFECT + TOOLTIP_COMMON_EFFECTS
+    private static final String ERROR_TOOLTIP_EFFECT = POPUP_SHADOW_EFFECT + TOOLTIP_COMMON_EFFECTS
             + "-fx-background-color: FBEFEF; -fx-text-fill: cc0033; -fx-border-color:cc0033;"; //$NON-NLS-1$
 
-    private static final String WARNING_TOOLTIP_EFFECT = SHADOW_EFFECT + TOOLTIP_COMMON_EFFECTS
+    private static final String WARNING_TOOLTIP_EFFECT = POPUP_SHADOW_EFFECT + TOOLTIP_COMMON_EFFECTS
             + "-fx-background-color: FFFFCC; -fx-text-fill: CC9900; -fx-border-color: CC9900;"; //$NON-NLS-1$
 
     /**

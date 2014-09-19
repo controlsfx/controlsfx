1a7b245a5c1651bb92914c3faabb66b95ef3893d
Jonathan Giles <jonathan@jonathangiles.net>
2014-09-19 14:53:25 +1200

    Fix exception in decoration code

--- controlsfx/src/main/java/org/controlsfx/control/decoration/Decorator.java
+++ controlsfx/src/main/java/org/controlsfx/control/decoration/Decorator.java
@@ -189,12 +189,12 @@ public class Decorator {
                 if (_pane == null) {
                     currentlyInstallingScenes.add(scene);
                     _pane = new DecorationPane();
+                    Node oldRoot = scene.getRoot();
+                    ImplUtils.injectAsRootPane(scene, _pane, true);
+                    _pane.setRoot(oldRoot);
+                    currentlyInstallingScenes.remove(scene);
                 }
                 
-                Node oldRoot = scene.getRoot();
-                ImplUtils.injectAsRootPane(scene, _pane, true);
-                _pane.setRoot(oldRoot);
-                currentlyInstallingScenes.remove(scene);
                 task.accept(_pane);
             };
             

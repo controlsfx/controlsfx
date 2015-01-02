c538721b69071179c7da8e9a7e73b5893dd1bc97
Thomas Nield <thomasnield@live.com>
2015-01-01 18:23:48 -0600

    attempt to implement listeners that update filter items when data changes. Add/Removes are registered, but changes in fields are not working.

---
+++ controlsfx/src/main/java/org/controlsfx/control/table/FilterPanel.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2014, ControlsFX
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ControlsFX, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CONTROLSFX BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.controlsfx.control.table;
+
+import javafx.beans.Observable;
+import javafx.collections.ListChangeListener;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Side;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import org.controlsfx.control.CheckListView;
+
+import com.sun.javafx.scene.control.skin.NestedTableColumnHeader;
+import com.sun.javafx.scene.control.skin.TableColumnHeader;
+import com.sun.javafx.scene.control.skin.TableViewSkin;
+
+
+public final class FilterPanel<T> extends Pane {
+	
+	private final ColumnFilter<T> columnFilter;
+	private final CheckListView<Object> checkListView = new CheckListView<>();
+	private final TextField searchBox = new TextField("Search...");
+	
+	FilterPanel(ColumnFilter<T> columnFilter) { 
+		this.columnFilter = columnFilter;
+		
+		VBox vBox = new VBox();
+		vBox.setPadding(new Insets(3));
+		
+		vBox.getChildren().add(searchBox);
+		searchBox.setPadding(new Insets(0,0,10,0));		
+		
+		vBox.getChildren().add(checkListView);
+		
+		Button applyBttn = new Button("APPLY");
+		applyBttn.setOnAction(e -> columnFilter.getTableFilter().executeFilter());
+		vBox.getChildren().add(applyBttn);
+		
+		this.getChildren().add(vBox);
+	}
+	
+	private void buildCheckList() { 
+		checkListView.itemsProperty().get().clear();
+		checkListView.itemsProperty().get().addAll(columnFilter.getDistinctValues());
+	}
+
+	public static <T> MenuItem getInMenuItem(ColumnFilter<T> columnFilter) { 
+		CustomMenuItem menuItem = new CustomMenuItem();
+		FilterPanel<T> filterPanel = new FilterPanel<T>(columnFilter);
+		
+		filterPanel.buildCheckList();
+		
+		ListChangeListener<Object> checkListListener = l -> columnFilter.getSelectedDistinctValues().setAll(filterPanel.checkListView.getSelectionModel().getSelectedItems());
+		ListChangeListener<T> dataChangeListener = l -> filterPanel.buildCheckList();
+		
+		filterPanel.checkListView.getSelectionModel().getSelectedItems().addListener(checkListListener);
+		columnFilter.getTableFilter().getTableView().getItems().addListener(dataChangeListener);
+		columnFilter.getTableColumn().setOnEditCommit(e -> { 
+			columnFilter.rebuildAllVals(); 
+			filterPanel.buildCheckList();
+		});
+		
+		menuItem.contentProperty().set(filterPanel);
+		
+		columnFilter.getTableFilter().getTableView().skinProperty().addListener((w, o, n) -> {
+		    if (n instanceof TableViewSkin) {
+		        TableViewSkin<?> skin = (TableViewSkin<?>) n;
+		            checkChangeContextMenu(skin, columnFilter.getTableColumn());
+		    }
+		});
+		
+		return menuItem;
+	}
+	
+	/* Methods below helps will anchor the context menu under the column */
+	private static void checkChangeContextMenu(TableViewSkin<?> skin, TableColumn<?, ?> column) {
+	    NestedTableColumnHeader header = skin.getTableHeaderRow()
+	            .getRootHeader();
+	    header.getColumnHeaders().addListener((Observable obs) -> changeContextMenu(header,column));
+	    changeContextMenu(header, column);
+	}
+
+	private static void changeContextMenu(NestedTableColumnHeader header, TableColumn<?, ?> column) {
+	    TableColumnHeader headerSkin = scan(column, header);
+	    if (headerSkin != null) {
+	        headerSkin.setOnContextMenuRequested(ev -> {
+	            ContextMenu cMenu = column.getContextMenu();
+	            if (cMenu != null) {
+	                cMenu.show(headerSkin, Side.BOTTOM, 5, 5);
+	            }
+	            ev.consume();
+	        });
+	    }
+	}
+
+	private static TableColumnHeader scan(TableColumn<?, ?> search,
+	        TableColumnHeader header) {
+	    // firstly test that the parent isn't what we are looking for
+	    if (search.equals(header.getTableColumn())) {
+	        return header;
+	    }
+
+	    if (header instanceof NestedTableColumnHeader) {
+	        NestedTableColumnHeader parent = (NestedTableColumnHeader) header;
+	        for (int i = 0; i < parent.getColumnHeaders().size(); i++) {
+	            TableColumnHeader result = scan(search, parent
+	                    .getColumnHeaders().get(i));
+	            if (result != null) {
+	                return result;
+	            }
+	        }
+	    }
+
+	    return null;
+	}
+}
---
+++ controlsfx/src/main/java/org/controlsfx/control/table/TableFilter.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2014, ControlsFX
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of ControlsFX, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CONTROLSFX BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.controlsfx.control.table;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public final class TableFilter<T> {
+	
+	private final TableView<T> tableView;
+	private final FilteredList<T> filteredList;
+	
+	private final ObservableList<ColumnFilter<T>> columnFilters = FXCollections.observableArrayList();
+	
+	private final Predicate<T> filterPredicate = v -> columnFilters.stream().filter(f -> f.isSelected(v) == false)
+			.findAny().isPresent() == false;
+	
+	private TableFilter(TableView<T> tableView) { 
+		this.tableView = tableView;
+		this.filteredList = new FilteredList<T>(tableView.getItems());
+		this.filteredList.setPredicate(v -> true);
+	}
+	public static <B> TableFilter<B> forTable(TableView<B> tableView) { 
+		TableFilter<B> tableFilter = new TableFilter<B>(tableView);
+		tableFilter.applyForAllColumns();
+		tableFilter.setFilterList();
+		return tableFilter;
+	}
+	
+	private void applyForAllColumns() { 
+		columnFilters.setAll(this.tableView.getColumns().stream()
+				.map(c -> ColumnFilter.getInstance(this, c)).collect(Collectors.toList()));
+	}
+	private void setFilterList() { 
+		tableView.setItems(filteredList);
+	}
+	public void executeFilter() { 
+		filteredList.setPredicate(filterPredicate);
+	}
+	public TableView<T> getTableView() { 
+		return tableView;
+	}
+	public ObservableList<ColumnFilter<T>> getColumnFilters() { 
+		return columnFilters;
+	}
+	public Optional<ColumnFilter<T>> getColumnFilter(TableColumn<T,?> tableColumn) { 
+		return columnFilters.stream().filter(f -> f.getTableColumn().equals(tableColumn)).findAny();
+	}
+	
+	
+}

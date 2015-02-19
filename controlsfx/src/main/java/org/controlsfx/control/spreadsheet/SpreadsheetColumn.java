174d27b7c5b5d8d34efa75355d85e8b5ab165b62
Samir Hadzic <samir.hadzic.pro@gmail.com>
2015-02-19 15:54:26 +0100

    SpreadsheetColumn update its contextMenu when fixedColumnsProperty is changing.

--- controlsfx/src/main/java/org/controlsfx/control/spreadsheet/SpreadsheetColumn.java
+++ controlsfx/src/main/java/org/controlsfx/control/spreadsheet/SpreadsheetColumn.java
@@ -32,6 +32,8 @@ import impl.org.controlsfx.spreadsheet.CellView;
 import java.util.List;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -113,11 +115,16 @@ public final class SpreadsheetColumn {
             column.setContextMenu(getColumnContextMenu());
         });
 
-        // When changing FixedColumns, we set header in order to add "." or ":"
-        spreadsheetView.getFixedColumns().addListener(updateTextListener);
+        // When changing frozen fixed columns, we need to update the ContextMenu.
+        spreadsheetView.fixingColumnsAllowedProperty().addListener(new ChangeListener<Boolean>() {
 
-        // When changing frozen fixed columns, we need to update the header.
-        spreadsheetView.fixingColumnsAllowedProperty().addListener(updateTextListener);
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                CellView.getValue(() -> {
+                    column.setContextMenu(getColumnContextMenu());
+                });
+            }
+        });
 
         // When ColumnsHeaders are changing, we update the text
         grid.getColumnHeaders().addListener(new InvalidationListener() {
@@ -287,15 +294,4 @@ public final class SpreadsheetColumn {
         }
         return true;
     }
-
-    /**
-     * Update the text in the header. So take the text and remove/add any "." or
-     * ":" if necessary.
-     */
-    private final InvalidationListener updateTextListener = new InvalidationListener() {
-        @Override
-        public void invalidated(Observable arg0) {
-            setText(getText());
-        }
-    };
 }

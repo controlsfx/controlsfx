e8f06b81b8f66b81fce7d5efdabb5727ebae3068
Samir Hadzic <samir.hadzic.pro@gmail.com>
2015-08-05 15:55:01 +0200

    Focus is set to the first cell when selecting all cells in SpreadsheetView.

--- controlsfx/src/main/java/impl/org/controlsfx/spreadsheet/TableViewSpanSelectionModel.java
+++ controlsfx/src/main/java/impl/org/controlsfx/spreadsheet/TableViewSpanSelectionModel.java
@@ -562,7 +562,9 @@ public class TableViewSpanSelectionModel extends
 
         if (tp != null) {
             select(tp.getRow(), tp.getTableColumn());
-            getTableView().getFocusModel().focus(tp.getRow(), tp.getTableColumn());
+            //Just like verticalHeader, the focus should be put on the 
+            //first cell to ease copy/paste operation.
+            getTableView().getFocusModel().focus(0, getTableView().getColumns().get(0));
         }
     }
 

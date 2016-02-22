2c8889c1323e4fa867604ec8581612ef1552ed28
Anirvan Sarkar <powers.anirvan@gmail.com>
2016-02-22 15:36:36 +0530

    Replace deprecated Window#impl_getWindows in JavaFX 8 with Window#getWindows in JavaFX 9.

--- controlsfx/src/main/java/org/controlsfx/tools/Utils.java
+++ controlsfx/src/main/java/org/controlsfx/tools/Utils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014, 2015, ControlsFX
+ * Copyright (c) 2014, 2016, ControlsFX
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,7 +26,7 @@
  */
 package org.controlsfx.tools;
 
-import java.util.Iterator;
+import java.util.List;
 
 import javafx.scene.Node;
 import javafx.stage.PopupWindow;
@@ -44,17 +44,14 @@ public class Utils {
      */
     public static Window getWindow(Object owner) throws IllegalArgumentException {
         if (owner == null) {
-            Window window = null;
             // lets just get the focused stage and show the dialog in there
-            @SuppressWarnings("deprecation")
-            Iterator<Window> windows = Window.impl_getWindows();
-            while (windows.hasNext()) {
-                window = windows.next();
+            List<Window> windows = Window.getWindows();
+            for (Window window : windows) {
                 if (window.isFocused() && !(window instanceof PopupWindow)) {
-                    break;
+                    return window;
                 }
             }
-            return window;
+            return null;
         } else if (owner instanceof Window) {
             return (Window) owner;
         } else if (owner instanceof Node) {

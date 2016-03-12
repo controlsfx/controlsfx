8a09856754a81b0391fc7d916bd3bec7495e5d8b
Thomas Nield <thomasnield@live.com>
2016-03-12 07:34:29 -0600

    Forgot CSS file

---
+++ controlsfx/src/main/resources/impl/org/controlsfx/table/tablefilter.css
@@ -0,0 +1,16 @@
+.column-filter .context-menu {
+    -fx-background-color: white;
+}
+
+.column-filter .context-menu:focused {
+    -fx-background-color: white;
+}
+
+.column-filter .custom-menu-item {
+    -fx-background-color: white;
+    -fx-padding: 0;
+}
+
+.column-filter .custom-menu-item:focused {
+    -fx-background-color: white;
+}

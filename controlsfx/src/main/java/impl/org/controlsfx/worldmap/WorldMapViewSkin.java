a7fafc87b9382831407754b9048190fd1085e596
Dirk Lemmermann <dlemmermann@gmail.com>
2016-11-21 17:51:45 +0100

    Initial commit.

--- controlsfx/src/main/java/impl/org/controlsfx/worldmap/WorldMapViewSkin.java
+++ controlsfx/src/main/java/impl/org/controlsfx/worldmap/WorldMapViewSkin.java
@@ -1,13 +1,12 @@
 package impl.org.controlsfx.worldmap;
 
 import javafx.collections.*;
-import javafx.scene.CacheHint;
+import javafx.scene.Group;
 import javafx.scene.control.SkinBase;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
-import org.controlsfx.control.ScalableContentPane;
 import org.controlsfx.control.WorldMapView;
 
 import java.util.*;
@@ -17,18 +16,11 @@ public class WorldMapViewSkin extends SkinBase<WorldMapView> {
 
     private static final double PREFERRED_WIDTH = 1009;
     private static final double PREFERRED_HEIGHT = 665;
-    private static final double MINIMUM_WIDTH = 100;
-    private static final double MINIMUM_HEIGHT = 66;
-    private static final double MAXIMUM_WIDTH = 2018;
-    private static final double MAXIMUM_HEIGHT = 1330;
     private static double MAP_OFFSET_X = -PREFERRED_WIDTH * 0.0285;
     private static double MAP_OFFSET_Y = PREFERRED_HEIGHT * 0.195;
-    private static final double ASPECT_RATIO = PREFERRED_HEIGHT / PREFERRED_WIDTH;
-    private double width;
-    private double height;
 
     protected Pane countryPane;
-    protected ScalableContentPane scalableContentPane;
+    protected Group group;
     protected Map<String, List<WorldMapView.CountryPath>> countryPaths;
     protected ObservableMap<WorldMapView.Location, Shape> locationMap;
 
@@ -39,15 +31,11 @@ public class WorldMapViewSkin extends SkinBase<WorldMapView> {
         countryPaths = new HashMap<>();
         locationMap = FXCollections.observableHashMap();
 
-        countryPane = new Pane();
+        group = new Group();
+        group.setManaged(false);
 
-        scalableContentPane = new ScalableContentPane(countryPane);
-        scalableContentPane.setPrefWidth(PREFERRED_WIDTH);
-        scalableContentPane.setPrefHeight(PREFERRED_HEIGHT);
-        scalableContentPane.setAutoRescale(true);
-        scalableContentPane.setFitToHeight(true);
-        scalableContentPane.setFitToWidth(true);
-        scalableContentPane.widthProperty().addListener(it -> System.out.println(scalableContentPane.getWidth() + " / " + scalableContentPane.getHeight()));
+        countryPane = new Pane();
+        countryPane.getChildren().add(group);
 
         ListChangeListener<? super WorldMapView.Location> locationsListener = change -> {
             while (change.next()) {
@@ -70,7 +58,7 @@ public class WorldMapViewSkin extends SkinBase<WorldMapView> {
         });
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(scalableContentPane);
+        borderPane.setCenter(countryPane);
 
         getChildren().add(borderPane);
 
@@ -100,7 +88,7 @@ public class WorldMapViewSkin extends SkinBase<WorldMapView> {
     }
 
     private void buildView() {
-        countryPane.getChildren().clear();
+        group.getChildren().clear();
 
         if (Double.compare(getSkinnable().getPrefWidth(), 0.0) <= 0 || Double.compare(getSkinnable().getPrefHeight(), 0.0) <= 0 ||
                 Double.compare(getSkinnable().getWidth(), 0.0) <= 0 || Double.compare(getSkinnable().getHeight(), 0.0) <= 0) {
@@ -113,33 +101,27 @@ public class WorldMapViewSkin extends SkinBase<WorldMapView> {
 
         for (WorldMapView.Country country : WorldMapView.Country.values()) {
             List<WorldMapView.CountryPath> paths = country.getPaths();
-            countryPane.getChildren().addAll(paths);
+            group.getChildren().addAll(paths);
             countryPaths.put(country.name(), paths);
         }
     }
 
-//        @Override
-//    protected void layoutChildren(double contentX, double contentY, double contentWidth, double contentHeight) {
-//                super.layoutChildren(contentX, contentY, contentWidth, contentHeight);
-//            scalableContentPane.requestScale();
-//        System.out.println("laying out");
-//        width = getSkinnable().getWidth() - getSkinnable().getInsets().getLeft() - getSkinnable().getInsets().getRight();
-//        height = getSkinnable().getHeight() - getSkinnable().getInsets().getTop() - getSkinnable().getInsets().getBottom();
-//
-//        if (ASPECT_RATIO * width > height) {
-//            width = 1 / (ASPECT_RATIO / height);
-//        } else if (1 / (ASPECT_RATIO / height) > width) {
-//            height = ASPECT_RATIO * width;
-//        }
-//
-//        if (width > 0 && height > 0) {
-//            countryPane.setCache(true);
-//            countryPane.setCacheHint(CacheHint.SCALE);
-//
-////            scalableContentPane.setMaxSize(width, height);
-////            scalableContentPane.setPrefSize(width, height);
-//
-//            countryPane.setCache(false);
-//        }
-//    }
+    @Override
+    protected void layoutChildren(double contentX, double contentY, double contentWidth, double contentHeight) {
+        super.layoutChildren(contentX, contentY, contentWidth, contentHeight);
+
+        double prefWidth = group.prefWidth(-1);
+        double prefHeight = group.prefHeight(-1);
+
+        double scaleX = contentWidth / prefWidth;
+        double scaleY = contentHeight / prefHeight;
+
+        double scale = Math.min(scaleX, scaleY);
+
+        group.setScaleX(scale);
+        group.setScaleY(scale);
+
+        group.setLayoutX((contentWidth - prefWidth) / 2);
+        group.setLayoutY((contentHeight - prefHeight) / 2);
+    }
 }

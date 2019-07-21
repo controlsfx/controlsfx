6847ebfa2035da67b9cf4759e479917a34878ffb
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-07-21 09:07:11 +0530

    Travis build should fail if decryption is unsuccessful

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 openssl aes-256-cbc -K $encrypted_ad8c90635771_key -iv $encrypted_ad8c90635771_iv -in .ci/sonatype.gpg.enc -out sonatype.gpg -d
+if [[ ! -s sonatype.gpg ]]
+   then echo "Decryption failed."
+   exit 1
+fi
+
 ./gradlew uploadPublished --info -PsonatypeUsername=$SONATYPE_USERNAME -PsonatypePassword=$SONATYPE_PASSWORD -Psigning.keyId=$GPG_KEY_ID -Psigning.password=$GPG_KEY_PASSPHRASE -Psigning.secretKeyRingFile=/home/travis/build/controlsfx/controlsfx/sonatype.gpg

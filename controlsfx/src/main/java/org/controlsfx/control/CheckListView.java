9269b209834d9ee454009cdb10e25b065d315f25
Sam H <sam.hutchins@screamingfrog.co.uk>
2019-12-18 10:43:50 +0000

    NPE in CheckListView #1219
    Correct format for copyright dates

--- controlsfx/src/main/java/org/controlsfx/control/CheckListView.java
+++ controlsfx/src/main/java/org/controlsfx/control/CheckListView.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013, 2018, 2019 ControlsFX
+ * Copyright (c) 2013, 2019 ControlsFX
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

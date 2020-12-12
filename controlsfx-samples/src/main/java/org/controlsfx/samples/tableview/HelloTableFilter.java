d1bad9cdaa05b67eb46a36c8f3ff19bbd52081a7
Mason Chou <masonchou95035@gmail.com>
2020-12-11 20:26:34 -0800

    specify the imports from javafx package

--- controlsfx-samples/src/main/java/org/controlsfx/samples/tableview/HelloTableFilter.java
+++ controlsfx-samples/src/main/java/org/controlsfx/samples/tableview/HelloTableFilter.java
@@ -28,11 +28,14 @@ package org.controlsfx.samples.tableview;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Node;
-import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.controlsfx.ControlsFXSample;
 import org.controlsfx.control.table.TableFilter;

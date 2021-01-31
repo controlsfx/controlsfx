83db801b4e6813a6c8ed1a73e4fc21214c74d85d
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-02-01 00:26:34 +0530

    Update license header year

--- fxsampler/src/main/java/fxsampler/util/SampleScanner.java
+++ fxsampler/src/main/java/fxsampler/util/SampleScanner.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013, 2020, ControlsFX
+ * Copyright (c) 2013, 2021, ControlsFX
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -354,7 +354,4 @@ public class SampleScanner {
         }
         return clazz;
     }
-
-
-
 }

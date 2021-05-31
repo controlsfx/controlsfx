24b835f85eba72ad30e2d4d009f2327b47ff48da
Steffen Metzner <smetzner@axetrading.com>
2021-05-31 20:43:51 +0200

    #1364: unit test for index out of bounds exception when removing items from the underlaying list that are selected

--- controlsfx/src/test/java/org/controlsfx/control/tableview2/TableView2SelectionTest.java
+++ controlsfx/src/test/java/org/controlsfx/control/tableview2/TableView2SelectionTest.java
@@ -4,6 +4,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
@@ -18,6 +19,7 @@ import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class TableView2SelectionTest extends FxRobot {
@@ -61,6 +63,7 @@ public class TableView2SelectionTest extends FxRobot {
         tableView.setMaxWidth(TABLE_WIDTH);
         tableView.setMaxHeight(TABLE_HEIGHT);
         tableView.setPrefSize(TABLE_WIDTH, TABLE_HEIGHT);
+        tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
         tableView.getColumns()
                 .setAll(IntStream.range(0, NUMBER_OF_COLUMNS)
@@ -108,11 +111,32 @@ public class TableView2SelectionTest extends FxRobot {
             this.data.add(0, new RowItem(6, NUMBER_OF_COLUMNS));
         });
 
-        RowItem selectedItem = this.tableView.getSelectionModel()
+        RowItem actualSelectedItem = this.tableView.getSelectionModel()
                 .getSelectedItem();
-        RowItem secondItemInList = this.data.get(1);
+        RowItem expectedItem = this.data.get(1);
 
-        assertEquals(selectedItem, secondItemInList);
+        assertEquals(expectedItem, actualSelectedItem);
+    }
+
+    @Test
+    public void shouldRemoveItemsFromSelection_When_elementsAreRemovedFromTheUnderlyingList() {
+        for (int i = 0; i < 5; i++) {
+            data.add(0, new RowItem(5 - i, NUMBER_OF_COLUMNS));
+        }
+        waitUntilTableIsShown();
+
+        this.interact(() -> {
+            this.tableView.getSelectionModel().selectAll();
+        });
+
+        this.interact(() -> {
+            this.data.remove(0, 3);
+        });
+
+        final Object[] actualSelectedItems = this.tableView.getSelectionModel()
+                .getSelectedItems().toArray();
+        final Object[] expectedItems = this.data.toArray();
+        assertArrayEquals(expectedItems, actualSelectedItems);
     }
 
 }

d350f7d21306fc8341104b8daa05adc7b03cd28b
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2024-03-07 14:26:14 +0530

    avoid wilcard import

--- controlsfx/src/test/java/org/controlsfx/control/CheckBitSetModelBaseTest.java
+++ controlsfx/src/test/java/org/controlsfx/control/CheckBitSetModelBaseTest.java
@@ -37,7 +37,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CheckBitSetModelBaseTest {
 

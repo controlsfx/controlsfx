84c7b44faa7ecad7a5a5edb6910ba0eeb415d4e9
Aram <jixxed@hotmail.com>
2025-10-31 10:13:36 +0100

    fix: div by zero in SegmentedBar (#1601)
    Co-authored-by: jixxed <jixxed@jixxed.nl>

---
+++ controlsfx/src/test/java/impl/org/controlsfx/skin/SegmentedBarSkinTest.java
@@ -0,0 +1,78 @@
+package impl.org.controlsfx.skin;
+
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import org.controlsfx.control.SegmentedBar;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.testfx.api.FxToolkit;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+@RunWith(Parameterized.class)
+public class SegmentedBarSkinTest {
+
+    @Parameterized.Parameters(name = "{index}: {0} = x: {1}, y: {2}, w: {3}, h: {4} ")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{{Orientation.HORIZONTAL, 100.0, 100.0, 0.0, 100.0}, {Orientation.VERTICAL, 100.0, 200.0, 100.0, 0.0}});
+    }
+
+    public SegmentedBarSkinTest(Orientation orientation, double x, double y, double w, double h) {
+        this.orientation = orientation;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    private Orientation orientation;
+
+    private double x;
+    private double y;
+    private double w;
+    private double h;
+
+    @BeforeClass
+    public static void setupSpec() throws TimeoutException {
+        FxToolkit.registerPrimaryStage();
+    }
+
+    @AfterClass
+    public static void afterClass() throws TimeoutException {
+        FxToolkit.cleanupStages();
+    }
+
+    @Test
+    public void testLayoutChildrenDoNotResizeRelocateToNaN() {
+        SegmentedBar<SegmentedBar.Segment> control = new SegmentedBar<>();
+        control.setOrientation(orientation);
+        StackPane segmentView1 = new StackPane();
+        StackPane segmentView2 = new StackPane();
+        Map<SegmentedBar.Segment, Node> segmentNodes = Map.of(
+                new SegmentedBar.Segment(0.0), segmentView1,
+                new SegmentedBar.Segment(0.0), segmentView2
+        );
+        control.getSegments().addAll(segmentNodes.keySet());
+        control.setSegmentViewFactory(segmentNodes::get);
+        SegmentedBarSkin<SegmentedBar.Segment> segmentedBarSkin = new SegmentedBarSkin<>(control);
+
+        segmentedBarSkin.layoutChildren(100, 100, 100, 100);
+
+        Assert.assertEquals(x, segmentView1.getLayoutX(), 0.0);
+        Assert.assertEquals(y, segmentView1.getLayoutY(), 0.0);
+        Assert.assertEquals(w, segmentView1.getWidth(), 0.0);
+        Assert.assertEquals(h, segmentView1.getHeight(), 0.0);
+
+        Assert.assertEquals(x, segmentView2.getLayoutX(), 0.0);
+        Assert.assertEquals(y, segmentView2.getLayoutY(), 0.0);
+        Assert.assertEquals(w, segmentView2.getWidth(), 0.0);
+        Assert.assertEquals(h, segmentView2.getHeight(), 0.0);
+    }
+}

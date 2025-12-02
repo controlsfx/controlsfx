2fde3abd73000637d0a4b529569eac8cfd6e60d0
Abhinay Agarwal <abhinay_agarwal@live.com>
2025-12-02 16:39:42 +0530

    update license header

--- controlsfx/src/main/java/org/controlsfx/control/textfield/TextFields.java
+++ controlsfx/src/main/java/org/controlsfx/control/textfield/TextFields.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014, 2021, ControlsFX
+ * Copyright (c) 2014, 2025, ControlsFX
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
